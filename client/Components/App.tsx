298d5ca1af59f6726707aa86d54d061d792c9e9a
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2021-04-23 16:21:27 +1200

    add webpack, typescript in client folder

---
+++ client/Components/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+    return(
+        <h1>App has arrived</h1>
+    )
+}
+
+export default App
---
+++ client/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { render } from "react-dom"
+
+import App from './Components/App'
+
+document.addEventListener('DOMContentLoaded', () => {
+    render(
+        <App />,
+        document.getElementById("App")
+    )
+})
